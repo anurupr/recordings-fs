2929445e4fd0a472769a916c8168b7a6b5c36d1c
Anurup Raveendran <anurupraveendran@gmail.com>
2017-02-12 23:07:29 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+
+const gitPull = () => {
+  const
+      spawn = require( 'child_process' ).spawn,
+      git = spawn( 'git', [ 'pull'] ,"C:\\");
+
+  git.stdout.on( 'data', data => {
+      console.log( `stdout: ${data}` );
+  });
+
+  git.stderr.on( 'data', data => {
+      console.log( `stderr: ${data}` );
+  });
+
+  git.on( 'close', code => {
+      console.log( `child process exited with code ${code}` );
+  });
+}
+
+
+const app = express();
+
+
+app.post('/repo', (req, res, rext) => {
+  gitPull();
+})
+
+
+app.listen('8999', '0.0.0.0', () => {
+  console.log('started');
+});
